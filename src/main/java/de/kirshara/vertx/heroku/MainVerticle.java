f18f1368318fc85373c3150dc89419539892ae65
Tobias Doerner <tobias.doerner@edag-bfft.com>
2020-04-30 12:13:58 +0200

    Refactored config retrieving process

--- src/main/java/de/kirshara/vertx/heroku/MainVerticle.java
+++ src/main/java/de/kirshara/vertx/heroku/MainVerticle.java
@@ -1,50 +1,22 @@
 package de.kirshara.vertx.heroku;
 
-import io.vertx.config.ConfigRetriever;
-import io.vertx.config.ConfigRetrieverOptions;
-import io.vertx.config.ConfigStoreOptions;
+import de.kirshara.vertx.heroku.config.ConfigManager;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Promise;
-import io.vertx.core.json.JsonObject;
 
 public class MainVerticle extends AbstractVerticle {
 
-    private ConfigRetriever configRetriever;
-    private JsonObject config;
+    private ConfigManager configManager;
 
     @Override
     public void start(Promise<Void> startPromise) {
-        // Reading config params from resoucres/config.json file
-        final ConfigStoreOptions configStoreFileOptions = new ConfigStoreOptions()
-            .setType("file")
-            .setOptional(true)
-            .setConfig(new JsonObject().put("path", "config.json"));
-
-        // Reading the environment variables
-        final ConfigStoreOptions configStoreSysOptions = new ConfigStoreOptions()
-            .setType("sys")
-            .setOptional(true);
-
-        // Reading the Config Vars which are defined in the settings of the application on Heroku
-        final ConfigStoreOptions configStoreEnvOptions = new ConfigStoreOptions()
-            .setType("env")
-            .setOptional(true);
-
-        final ConfigRetrieverOptions configRetrieverOptions = new ConfigRetrieverOptions()
-            .addStore(configStoreFileOptions)
-            .addStore(configStoreSysOptions)
-            .addStore(configStoreEnvOptions);
-
-        configRetriever = ConfigRetriever.create(vertx, configRetrieverOptions);
-
-        configRetriever.getConfig(json -> {
+        configManager = ConfigManager.getDefaultConfigManager(vertx);
+        configManager.getConfig().setHandler(json -> {
             if (json.succeeded()) {
-                config = json.result();
-
                 vertx.deployVerticle(
                     HttpServerVerticle::new,
-                    new DeploymentOptions().setConfig(config), ar -> {
+                    new DeploymentOptions().setConfig(json.result()), ar -> {
                         if (ar.succeeded()) {
                             startPromise.complete();
                         } else {
@@ -59,7 +31,7 @@ public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void stop() throws Exception {
-        configRetriever.close();
+        configManager.close();
         super.stop();
     }
 }
